ca9cbe0aa0f3985f7a9db7988414de8121925e4d
stino-x <107426247+stino-x@users.noreply.github.com>
2023-09-06 16:15:58 +0200

    desktop design

--- src/Components/Rocket/RocketContainer.js
+++ src/Components/Rocket/RocketContainer.js
@@ -1,14 +1,20 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
+/* eslint-disable react/no-array-index-key */
+import React, { useEffect, useContext } from 'react';
+import { useDispatch } from 'react-redux';
 import { Container } from 'react-bootstrap';
-import RocketList from './RocketList';
-import { fetchRocketsData } from '../../redux/rocket/rocketSlice';
+import { BsFillArrowRightCircleFill } from 'react-icons/bs';
+// import RocketList from './RocketList'
 import './Rocket.css';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { fetchRocketsData } from '../../redux/rocket/rocketSlice';
+import { UserContext } from '../ContextProvider/UserContextProvider';
 
 let saveReservedAPI = false;
 
 const RocketContainer = () => {
-  const rockets = useSelector((state) => state.rocket);
+  // const rockets = useSelector((state) => state.rocket);
+  const { dummyArray } = useContext(UserContext);
 
   const dispatch = useDispatch();
 
@@ -21,9 +27,26 @@ const RocketContainer = () => {
   }, []);
 
   return (
-    <Container>
-      <RocketList rockets={rockets} />
-    </Container>
+    <>
+      <div>selected continent</div>
+      <Container className="contaner">
+        <Row id="menu-header">
+          <Col>Menu header</Col>
+        </Row>
+        {dummyArray.map((item, position) => (
+          <Row className="row-1" md={4} key={position}>
+            <Col>Country</Col>
+            <Col md={6} sm={6} />
+            <Col>
+              3 of 3
+              <span className="arrow-container">
+                <BsFillArrowRightCircleFill />
+              </span>
+            </Col>
+          </Row>
+        ))}
+      </Container>
+    </>
   );
 };
 
